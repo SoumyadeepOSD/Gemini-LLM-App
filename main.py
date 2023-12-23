f4179cc6ae3c2046ca9485c2c4c31c53f3ce7450
SoumyadeepOSD <opencontainer255@gmail.com>
2023-12-23 16:04:43 +0530

    Development process started

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ API_KEY = st.secrets["API_KEY"]
 model = genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel('gemini-pro')
 
-new_title = '<p style="font-family:sans-serif; color:Green; font-size: 42px;">LLM App, powered by Gemini-pro</p>'
+new_title = '<p style="font-family:sans-serif; color:Green; font-size: 42px;">LLM App, powered by Gemini-pro(Development)</p>'
 st.markdown(new_title, unsafe_allow_html=True)
 
 prompt = st.text_input("Enter your query here")
